053ef8bc947b06ea9aa2a912176b3de09d2ecd70
AndreasBrochs <andreas.brochs@outlook.com>
2021-03-24 10:00:15 +0100

    buttons is showed conditionally if you are loged in or not. also added some profile information and picture

--- src/components/LogInBtn.js
+++ src/components/LogInBtn.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LogInBtn = () => {
-    const { loginWithRedirect } = useAuth0();
-    return (
-        <button onClick={() => loginWithRedirect()}>
-            Log In
-        </button>
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <button onClick={() => loginWithRedirect()}>Log In</button>
     )
-}
+  );
+};
 
 export default LogInBtn;
