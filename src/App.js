c709abc622ca7a18e4612082e8a6b8f9accbc557
AndreasBrochs <andreas.brochs@outlook.com>
2021-03-24 09:48:40 +0100

    showing user information when loged in

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import "./App.css";
 import LogInBtn from "./components/LogInBtn";
 import LogOutBtn from "./components/LogOutBtn";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Fragment>
       <LogInBtn />
       <LogOutBtn />
+      <Profile/>
     </Fragment>
   );
 }
