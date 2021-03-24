624373d55cab65e67f69115ab78448a2b2464d09
AndreasBrochs <andreas.brochs@outlook.com>
2021-03-24 10:07:14 +0100

    added a loading screen between logging in and out

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import LogInBtn from "./components/LogInBtn";
 import LogOutBtn from "./components/LogOutBtn";
 import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <Fragment>
       <LogInBtn />
       <LogOutBtn />
-      <Profile/>
+      <Profile />
     </Fragment>
   );
 }
