6f70fe103c93367d51f37131901a1b210048c100
AndreasBrochs <andreas.brochs@outlook.com>
2021-03-24 11:24:40 +0100

    secure routs, a navbar and some simple styling

---
+++ src/components/LandingPage.js
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <p>Welcome, hi, hello!</p>
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
@@ -18,4 +18,6 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default withAuthenticationRequired(Profile, {
+  onRedirecting: () => <div>Redirecting you to the login page...</div>,
+});
